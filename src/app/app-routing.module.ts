55dfcf87593d824545b16924164570301c3975b3
eruj22 <prci.prnaver2@gmail.com>
2021-12-14 10:42:10 +0100

    basic routing and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrewComponent } from './pages/crew/crew.component';
+import { DestinationComponent } from './pages/destination/destination.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'destination', component: DestinationComponent },
+  { path: 'crew', component: CrewComponent },
+  { path: 'technology', component: TechnologyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DestinationComponent } from './pages/destination/destination.component';
+import { CrewComponent } from './pages/crew/crew.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    DestinationComponent,
+    CrewComponent,
+    TechnologyComponent
   ],
   imports: [
     BrowserModule,
